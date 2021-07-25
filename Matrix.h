51da1a9724a2eaebbe1fa8510c94d21b5287d51e
YaroslavZovsky06 <87338219+YaroslavZovsky06@users.noreply.github.com>
2021-07-25 13:46:39 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,15 +1,10 @@
 #ifndef MATRIX_H
 #define MATRIX_H
-#include <ctime>
-#include <cstdlib>
 #include <iostream>
 #include <cmath>
-#include <QDebug>
 
 class Matrix {
 private:
-    int r;
-    int c;
     int **mat;
     int** reduceMatrix(int** mat, int n, int x, int y);
     int calculateRecursiveDeterminant(int** mat, int n);
@@ -17,7 +12,7 @@ public:
     Matrix(int n);
     Matrix(int r, int c);
     void setCell(int i, int j, int x);
-    int calculateRecursiveDeterminant();
+    int calculateRecursiveDeterminant(int n);
 
 
 };
