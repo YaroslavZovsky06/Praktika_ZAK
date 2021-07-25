08aca7bf725b5cb5ce348ff3805cebb60f806f80
YaroslavZovsky06 <87338219+YaroslavZovsky06@users.noreply.github.com>
2021-07-25 13:41:11 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,82 +21,74 @@ MainWindow::MainWindow(QString title, int n){
     central->setLayout(frameLayout); //задает окно макета сетки
     setCentralWidget(central); //установите фрейм как главный
     frameLayout->addWidget(mainFrame); //добавляет фрейм рабочей детали в Главное окно
-    QVBoxLayout *mainLayout = new QVBoxLayout;
+    QVBoxLayout *mainLayout = new QVBoxLayout;//выстраивает виджеты по вертикали
     QStackedLayout *stackedLayout = new QStackedLayout; //макет стеки
     mainFrame->setLayout(mainLayout); //добавляет все операции с макетом и рамкой
-    QWidget *determinat = new QWidget();
-
-    stackedLayout->addWidget(determinat);
+    QWidget *determinat = new QWidget();//создание окна вычисления определителя
+    stackedLayout->addWidget(determinat);//добавляет виджеты окна
     QWidget *matrixFrame = new QWidget(determinat); //создание фрейма массива
-    QVBoxLayout *determinatLayout = new QVBoxLayout;
-    determinat->setLayout(determinatLayout);
-    determinatLayout->addWidget(matrixFrame);
-    mainLayout->addLayout(stackedLayout);
+    QVBoxLayout *determinatLayout = new QVBoxLayout;//выстраивает виджеты по вертикали
+    determinat->setLayout(determinatLayout);//добавляет все операции с виджетами
+    determinatLayout->addWidget(matrixFrame);//добавляетфрейм массива
+    mainLayout->addLayout(stackedLayout);//добавляет макет
 
 
-    QWidget *buttonpanel = new QWidget;
-    QLabel *descr = new QLabel("Insert matrix dimension:");
-    QLineEdit *dim = new QLineEdit();
-
-    QPushButton *resize = new QPushButton("Resize");
-    QPushButton *calc = new QPushButton("Calculate");
-    dim->setFixedWidth(50);
-
-    QHBoxLayout *buttonpanelLayout = new QHBoxLayout;
-    QHBoxLayout *buttonpanelLayoutTop = new QHBoxLayout;
-    QWidget *buttonpanelTop = new QWidget;
-    buttonpanel->setLayout(buttonpanelLayout);
-    buttonpanelLayout->setSizeConstraint(QLayout::SetFixedSize);
-    buttonpanelLayout->addWidget(descr);
-    buttonpanelLayout->addWidget(dim);
-    buttonpanelLayout->addWidget(resize);
-    buttonpanelLayout->addWidget(calc);
-    buttonpanelTop->setLayout(buttonpanelLayoutTop);
-    buttonpanelLayoutTop->addWidget(buttonpanel);
-
-
+    QWidget *buttonpanel = new QWidget; //создание панели с вводом размерности матрицы и кнопками
+    QLabel *descr = new QLabel("Insert matrix dimension:");//подпись перед вводом размерноси матрицы
+    QLineEdit *dim = new QLineEdit();//создание поля для ввода размерности
+    QPushButton *resize = new QPushButton("Resize");//кнопка которая создает матрицу нужной размерности
+    QPushButton *calc = new QPushButton("Calculate");//кнопка которая вычисляет определитель
+    dim->setFixedWidth(50);//размер поля для ввода размерности матрицы
+    QHBoxLayout *buttonpanelLayout = new QHBoxLayout;//выстраивает панель горизонтально
+    buttonpanel->setLayout(buttonpanelLayout);//добавляет все операции с панелью
+    buttonpanelLayout->addWidget(descr);//добавляет лейбл
+    buttonpanelLayout->addWidget(dim);//добавляет ввод размерности матрицы
+    buttonpanelLayout->addWidget(resize);//добавляет кнопку изменить размерность
+    buttonpanelLayout->addWidget(calc);//добавляет кногмку вычисления
+    buttonpanel->setLayout(buttonpanelLayout);//создает панель
+    buttonpanelLayout->addWidget(buttonpanel);//добавляет панель
     dim->setText("3");
-    grid = new QGridLayout();
+
+    grid = new QGridLayout();//для создания сетки
     for(int i=0;i<n;i++){
         matrix.append(QVector<QLineEdit*>());
         for(int j=0;j<n;j++){
             matrix[i].append(new QLineEdit);
-        }
+        }//создает матрицу заданного размера
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            matrix[i][j]->setFixedSize(30,30);
+            matrix[i][j]->setFixedSize(30,30);//размерность ячеек матрицы
             matrix[i][j]->setText("0");
-            matrix[i][j]->setAlignment(Qt::AlignCenter);
-            grid->addWidget(matrix[i][j],i,j);
+            matrix[i][j]->setAlignment(Qt::AlignCenter);//выравнивает текст по центру
+            grid->addWidget(matrix[i][j],i,j);//добавляет матрицу в сетку
         }
     }
-    grid->setSizeConstraint(QLayout::SetMaximumSize);
-    QHBoxLayout *detCentralizer = new QHBoxLayout();
-    matrixFrame->setLayout(detCentralizer);
-    QWidget *subMatrixFrame = new QWidget();
-    detCentralizer->addWidget(subMatrixFrame);
-    subMatrixFrame->setLayout(grid);
+    grid->setSizeConstraint(QLayout::SetMaximumSize);//выравнивание сетки
+    QHBoxLayout *detCentralizer = new QHBoxLayout();//выстраивает панель горизонтально
+    matrixFrame->setLayout(detCentralizer);//добавляет все операции с матрицей
+    QWidget *subMatrixFrame = new QWidget();//создает виджет матрицы
+    subMatrixFrame->setLayout(grid);//добавляет созданную матрицу
+    detCentralizer->addWidget(subMatrixFrame);//выводит матрицу
+\
 
-    QWidget *theoryFrame = new QWidget;
-    QHBoxLayout *theoryLayout = new QHBoxLayout;
-    QLineEdit *theoryLine = new QLineEdit();
-    theoryFrame->setLayout(theoryLayout);
-    theoryLayout->addWidget(theoryLine);
-    theoryLine->setFixedSize(400,200);
+    QWidget *theoryFrame = new QWidget;//создаем панель с текстом
+    QHBoxLayout *theoryLayout = new QHBoxLayout;//размещает панель горизонтально
+    QLineEdit *theoryLine = new QLineEdit();//создаем место для текста
+    theoryFrame->setLayout(theoryLayout);//добавляем панель с текстом
+    theoryLayout->addWidget(theoryLine);//добавляем на панель виджет
     theoryLine->setText("Задаём размер матрицы, вводим данные и находим определитель.");
+    determinatLayout->addWidget(theoryFrame);//выводит фрейм с текстом
 
-    determinatLayout->addWidget(theoryFrame);
+    QWidget *resultFrame = new QWidget;//создается панель ответа
+    QHBoxLayout *resultLayout = new QHBoxLayout;//размещает панель горизонтально
+    QLabel *resultLabel = new QLabel("");//создается лейбл с ответом
+    resultFrame->setLayout(resultLayout);//создает панель
+    resultLayout->addWidget(resultLabel);//добавляет панель
 
-    QWidget *resultFrame = new QWidget;
-    QHBoxLayout *resultLayout = new QHBoxLayout;
-    QLabel *resultLabel = new QLabel("");
-    resultFrame->setLayout(resultLayout);
-    resultLayout->addWidget(resultLabel);
-
-    determinatLayout->addWidget(buttonpanelTop);
-    determinatLayout->addWidget(matrixFrame);
-    determinatLayout->addWidget(resultFrame);
+    determinatLayout->addWidget(buttonpanel);//вывод панели кнопок
+    determinatLayout->addWidget(matrixFrame);//вывод матрицы после панели
+    determinatLayout->addWidget(resultFrame);//вывод результата
 
     this->connect(calc,&QPushButton::clicked,
         [this, resultLabel](){
@@ -119,11 +111,11 @@ void MainWindow::calculateDeterminant(QLabel *label){
     Matrix m(n);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            int x = matrix.at(i).at(j)->text().toInt();
-            m.setCell(i,j,x);
+            int x = matrix.at(i).at(j)->text().toInt();//введеные вручную значания выдаёт в формате int
+            m.setCell(i,j,x);//mat[i][j]=x
         }
     }
-    int det = m.calculateRecursiveDeterminant();
+    int det = m.calculateRecursiveDeterminant(n);
     label->setText("Determinat: " + QString::number(det));
     label->setAlignment(Qt::AlignCenter);
 
@@ -132,14 +124,14 @@ void MainWindow::calculateDeterminant(QLabel *label){
 
 
 void MainWindow::resizeDeterminantMatrix(QLineEdit* dim, QWidget *matrixFrame){
-    int x = dim->text().toInt();
+    int x = dim->text().toInt();//введеные вручную значание выдаёт в формате int
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             delete matrix[i][j];
         }
-    }
+    }//удаление старой матрицы
     matrix.clear();
-    n = x;
+    n = x;//заданный размер
     delete grid;
     grid = new QGridLayout();
     grid->setSizeConstraint(QLayout::SetMaximumSize);
@@ -148,13 +140,13 @@ void MainWindow::resizeDeterminantMatrix(QLineEdit* dim, QWidget *matrixFrame){
         for(int j=0;j<n;j++){
             matrix[i].append(new QLineEdit);
         }
-    }
+    }//создает матрицу заданного размера
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            matrix[i][j]->setFixedSize(30,30);
+            matrix[i][j]->setFixedSize(30,30);//размерность ячеек матрицы
             matrix[i][j]->setText("0");
-            matrix[i][j]->setAlignment(Qt::AlignCenter);
-            grid->addWidget(matrix[i][j],i,j);
+            matrix[i][j]->setAlignment(Qt::AlignCenter);//выравнивает текст по центру
+            grid->addWidget(matrix[i][j],i,j);//добавляет матрицу в сетку
         }
     }
     matrixFrame->setLayout(grid);
@@ -167,4 +159,3 @@ void MainWindow::resizeDeterminantMatrix(QLineEdit* dim, QWidget *matrixFrame){
 MainWindow::~MainWindow(){
     delete ui;
 }
-
