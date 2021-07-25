9d9d3712966d8d1eb548c5e6311f7102eb688df6
YaroslavZovsky06 <87338219+YaroslavZovsky06@users.noreply.github.com>
2021-07-25 13:52:15 +0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,13 +1,12 @@
 #include "Matrix.h"
 
 Matrix::Matrix(int n){
-    this->r = n;
-    this->c = n;
+
     mat = new int*[n];
     for(int i=0;i<n;i++){
         mat[i] = new int[n];
     }
-}
+}//создание матрицы
 
 
 void Matrix::setCell(int i, int j, int x){
@@ -20,7 +19,7 @@ int** Matrix::reduceMatrix(int** mat, int n, int x, int y){
     int** newMat = new int*[n-1];
     for(int i=0;i<(n-1);i++){
         newMat[i] = new int[n-1];
-    }
+    }//новая матрица
     int nj=0,ni=0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -35,7 +34,7 @@ int** Matrix::reduceMatrix(int** mat, int n, int x, int y){
         nj=0;
     }
     return newMat;
-}
+}//уменьшенная матрица
 
 int Matrix::calculateRecursiveDeterminant(int** mat, int n){
     if(n==1){
@@ -43,18 +42,13 @@ int Matrix::calculateRecursiveDeterminant(int** mat, int n){
     } else {
         int sum=0;
         for(int j=0;j<n;j++){
-            sum+=pow(-1,j)*mat[0][j]*calculateRecursiveDeterminant(reduceMatrix(mat,n,0,j),n-1);
+            sum+=pow(-1,j)*mat[0][j]*calculateRecursiveDeterminant(reduceMatrix(mat,n,0,j),n-1);//рекурсивное вычисление определителя
         }
         return sum;
     }
 }
 
-int Matrix::calculateRecursiveDeterminant(){
-    if(r==c)
-        return calculateRecursiveDeterminant(mat,r);
-    else
-        return 0;
+int Matrix::calculateRecursiveDeterminant(int n){
+
+        return calculateRecursiveDeterminant(mat,n);
 }
-
-
-
